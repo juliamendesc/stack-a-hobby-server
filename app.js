f75eff2602abc48cbfea25b16eebbb67c596233d
juliamendesc <juliamendesc@gmail.com>
2020-07-11 11:22:32 +0100

    heroku deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport     = require('passport');
 require('./configs/passport');
 
 mongoose
-  .connect('mongodb://localhost/stack-a-hobby-server', {useNewUrlParser: true})
+  .connect('mongodb://heroku_f60t7jcb:r66rc9384e2joh9ku5kgmrmik6@ds115543.mlab.com:15543/heroku_f60t7jcb', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
