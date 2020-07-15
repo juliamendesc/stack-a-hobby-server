d952ba6224f785d00dc54f1003e6c5d7338603e5
juliamendesc <juliamendesc@gmail.com>
2020-07-15 20:41:00 +0100

    changes in user edit

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-// const mongoose = require("mongoose");
-// const User = require("../models/user-model");
+const mongoose = require("mongoose");
+const User = require("../models/user-model");
 // const ratingModel = require("../models/rating-model");
 // const Comment = require("../models/comments-model");
-// const passportConfigs = require("../configs/passport");
-// const passport = require("passport");
+const passportConfigs = require("../configs/passport");
+const passport = require("passport");
 
 router.get("/user-details", (req, res) => {
   const userId = req.user._id;
