96c2df3db4ba14ce876ea38a73f924bff426995c
juliamendesc <juliamendesc@gmail.com>
2020-07-18 11:27:01 +0100

    added deployed website to cors policy

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 app.use(
   cors({
     credentials: true,
-    origin:['http://localhost:3000']
+    origin:['http://localhost:3000', 
+    'http://stack-a-hobby.s3-website-eu-west-1.amazonaws.com/']
   })
 )
 
