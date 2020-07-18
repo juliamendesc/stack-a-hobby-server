7cb4d913ad04c968458067ea247a8fc98041f017
juliamendesc <juliamendesc@gmail.com>
2020-07-18 17:06:18 +0100

    user model

--- models/user-model.js
+++ models/user-model.js
@@ -17,13 +17,6 @@ const userSchema = new Schema({
        ref: "Rating"
     }
  ],
-<<<<<<< HEAD
-  imageUrl: { type: String, 
-    // required: true, 
-    default: "https://res.cloudinary.com/jmc10/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1594582163/myFolder/default-user.png.png" }
-}, 
-
-=======
  comments: [
   {
     type: mongoose.Schema.Types.ObjectId,
@@ -31,7 +24,6 @@ const userSchema = new Schema({
   },
 ],
 },
->>>>>>> f511a01eb1c890ca7e12182f41bc1bc8b43ddb17
 {
   timestamps: {
     createdAt: 'createdAt',
