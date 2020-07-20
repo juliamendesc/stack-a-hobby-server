77ee54d8ad00f41cf73820f059056af8ac0f8b1d
juliamendesc <juliamendesc@gmail.com>
2020-07-20 14:02:28 +0100

    corrections in comments model

--- models/comments-model.js
+++ models/comments-model.js
@@ -11,7 +11,7 @@ const commentSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Course'
 	},
-	courseTitle: String,
+	title: String,
 },{
 	timestamps: {
 	  createdAt: 'createdAt',
