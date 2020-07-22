1079d75b0795583278c6dde5707c607f97bec28b
juliamendesc <juliamendesc@gmail.com>
2020-07-22 21:33:29 +0100

    changes in course-routes

--- routes/course-routes.js
+++ routes/course-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Course = require('../models/course-model');
+const User = require('../models/user-model');
 
 router.get('/courses', (req,res) => {
   Course.find()
