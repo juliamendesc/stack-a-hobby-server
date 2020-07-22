f175eaf2a3d80b7b6f515a72afb5249d05f67a96
juliamendesc <juliamendesc@gmail.com>
2020-07-22 21:50:35 +0100

    social login by julia

--- configs/passport.js
+++ configs/passport.js
@@ -52,9 +52,7 @@ passport.use(
             return;
           }
  
-          User.create({ googleId: profile.id, username: profile.displayName
-            // , imageUrl: profile.picture, firstName: profile.given_name,lastName: profile.family_name, email:profile.email 
-          })
+          User.create({ googleId: profile.id, username: profile.displayName, imageUrl: profile.picture, firstName: profile.given_name,lastName: profile.family_name, email:profile.email })
             .then(newUser => {
               done(null, newUser);
             })
